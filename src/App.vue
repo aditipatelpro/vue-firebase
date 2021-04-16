00092468a43475bc68d17051246a03c78acc33f0
aditipatelpro <aditipatelpro@gmail.com>
2021-04-16 15:44:17 -0500

    Removed boiler plate code and created basic wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,54 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>
+              mdi-account-group
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-action-text>
+            View Meetups
+          </v-list-item-action-text>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"
+        class="d-sm-none">
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="d-none d-sm-flex">
+        <v-btn plain>
+          <v-icon left>mdi-account-group</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+// import HelloWorld from './components/HelloWorld'
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    drawer: false
+  })
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>

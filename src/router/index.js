811943cbfe76ef8f7e4d1bc76c6ad65afdda4277
aditipatelpro <aditipatelpro@gmail.com>
2021-04-17 17:55:09 -0500

    Added paths for pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import Home from '../components/Home'
+import Meetups from '../components/Meetup/Meetups'
+import CreateMeetup from '../components/Meetup/CreateMeetup'
+import Profile from '../components/User/Profile'
+import Signin from '../components/User/Signin'
+import Signup from '../components/User/Signup'
 
 Vue.use(VueRouter)
 
@@ -9,6 +15,31 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/meetups',
+    name: 'Meetups',
+    component: Meetups
+  },
+  {
+    path: '/meetup/new',
+    name: 'CreateMeetup',
+    component: CreateMeetup
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
   },
   {
     path: '/about',

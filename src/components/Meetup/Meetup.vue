a34caf71ec0944a7d25eaaa111e7f052c392adb3
aditipatelpro <aditipatelpro@gmail.com>
2021-04-19 15:42:07 -0500

    Create single meetup page

---
+++ src/components/Meetup/Meetup.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>
+              Data Analysis Meetup
+          </v-card-title>
+          <v-img
+            src="https://static.rentcars.com/imagens/modules/localidade/about/739-desktop-location-description.png"
+            height="400px"
+          ></v-img>
+          <v-card-subtitle class="pb-0">
+            20th April 2021
+          </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary">
+              Register
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    <p> The Meetups Page</p>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-container fluid>
+            <v-row>
+              <v-col cols="5">
+                <v-img
+                  src="https://static.rentcars.com/imagens/modules/localidade/about/739-desktop-location-description.png"
+                  height="150px"
+                ></v-img>
+              </v-col>
+              <v-col>
+                <v-card-title>
+                  Meetup in Boston
+                </v-card-title>
+                <v-card-subtitle>
+                  20th April 2020
+                </v-card-subtitle>
+                <v-card-actions>
+                  <v-btn plain to="/meetups/1">
+                    View Meetup
+                  </v-btn>
+                </v-card-actions>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>

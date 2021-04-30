b90699bfadfea98c40fcac9d6b0b31477cdf12fd
aditipatelpro <aditipatelpro@gmail.com>
2021-04-30 16:31:55 -0500

    Handled null input values

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -65,11 +65,20 @@ export default {
   computed: {
     comparePasswords () {
       return this.password !== this.confirmPassword ? 'Passwords donot match' : ''
+    },
+    user () {
+      return this.$store.getters.user
+    }
+  },
+  watch: {
+    user (value) {
+      if (value !== null && value !== undefined) {
+        this.$router.push('/')
+      }
     }
   },
   methods: {
     onSignup () {
-      // console.log(this.email, this.password)
       this.$store.dispatch('signUserUp', { email: this.email, password: this.password })
     }
   }
